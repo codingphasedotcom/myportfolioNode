0f1ab12631ff6427e93e962012141f127ae444a6
Joe Santos Garcia <joesantosgarcia@gmail.com>
2017-10-09 01:24:51 -0400

    no message

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express()
 const webRoutes = require('./routes/web.js')
 const pug = require('pug')
 const path = require('path')
+require('dotenv').config()
 
 app.use(express.static('client/public'))
 app.set('view engine', 'pug')
@@ -13,3 +14,4 @@ app.use(webRoutes)
 app.listen(process.env.PORT || 3000, () => {
   console.log('Server is running on http://www.localhost:3000/ to stop server press control+c')
 })
+var pass = "johny2341"
